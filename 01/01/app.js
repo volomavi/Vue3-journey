13c8488824a98266843167665a3e13fccbf063c6
Ivan B <volomavi@hotmail.com>
2021-01-06 22:07:00 -0800

    and now in vue

--- 01/01/app.js
+++ 01/01/app.js
@@ -1,14 +1,29 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: '',
+    }
+  },
+  methods: {
+    addGoal(){
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    }
+  }
+}).mount('#app')
 
-function addGoal(){
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal(){
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
 
 
-buttonEl.addEventListener('click', addGoal)
+// buttonEl.addEventListener('click', addGoal)
