d704a2e3baf9e108c8354f170cabc42cc72205b2
Ivan B <volomavi@hotmail.com>
2021-01-21 00:09:07 -0800

    putting it together

---
+++ 04/app.js
@@ -0,0 +1,122 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      currentRound: 0,
+      winner: null,
+      battleLog: [],
+      logMessages: [],
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.playerHealth + "%" };
+    },
+    specialTimer() {
+      return this.currentRound % 3 !== 0;
+    },
+    gameOver() {
+      if (this.winner === "monster") {
+        return "You Lost.";
+      } else if (this.winner === "player") {
+        return "You Won!";
+      } else {
+        return "It's a draw";
+      }
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "player";
+      }
+    },
+  },
+  methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+      this.battleLog = [];
+      this.logMessages = [];
+    },
+    attackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.battleLog.unshift(
+        `Round ${this.currentRound}: You attacked the monster for ${attackValue} damage`
+      );
+      this.addLogMessage("player", "attack", attackValue);
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+      this.battleLog.unshift(
+        `Round ${this.currentRound}: The monster attacked you for ${attackValue} damage`
+      );
+      this.addLogMessage("monster", "attack", attackValue);
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.battleLog.unshift(
+        `Round ${this.currentRound}: You attacked the monster for ${attackValue} damage`
+      );
+      this.addLogMessage("player", "attack", attackValue);
+      this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(11, 18);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.battleLog.unshift(
+        `Round ${this.currentRound}: You healed for ${healValue} damage`
+      );
+      this.addLogMessage("player", "heal", healValue);
+      this.attackPlayer();
+    },
+    surrender() {
+      this.winner = "monster";
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
+    },
+  },
+});
+
+app.mount("#game");
